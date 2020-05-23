4b1f25f7a75ad1888ac8eb409d94478b92aad162
Michel Ludwig <michel.ludwig@gmail.com>
2020-05-23 10:49:02 +0200

    Fix typo

--- routes.js
+++ routes.js
@@ -31,7 +31,7 @@ app.get('/repo-data/', async (req, res) => {
             if(!config.repos[distro].hasOwnProperty(repo)) {
                 continue;
             }
-            reposToSend[repo] = {url: 'http://' + config.hostName + ':' + config.listenPort + '/cache/' + distro '/' + repo + '/$releasever/$basearch/'};
+            reposToSend[repo] = {url: 'http://' + config.hostName + ':' + config.listenPort + '/cache/' + distro + '/' + repo + '/$releasever/$basearch/'};
         }
         toSend[distro] = reposToSend;
     }
